1feb1e2a9d07657fab8d0055ffd7206e4cf893b0
moti <moti.kor@gmail.com>
2021-06-03 11:55:10 +0200

    Updates

---
+++ precache-manifest.2e8006c75cdf410bf0ff6eddc33f63e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c286f094e922e184a875af04f6068ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "894bdacb38ed183a5685",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "a6327bd739b328c50197",
+    "url": "./static/js/2.fe9a9649.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fe9a9649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894bdacb38ed183a5685",
+    "url": "./static/js/main.af61ad08.chunk.js"
+  },
+  {
+    "revision": "f195eb9a8bb21eec118c",
+    "url": "./static/js/runtime-main.c5eb5533.js"
+  }
+]);
